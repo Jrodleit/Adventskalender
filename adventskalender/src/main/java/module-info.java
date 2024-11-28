9b16d4c7e0a1f86259d2a67ed8b635d3344b57ed
jrodleit <jrodleit@htl-steyr.ac.at>
2024-11-28 10:23:11 +0100

    changed file structure

--- adventskalender/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module htl.steyr.adventskalender {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens htl.steyr.adventskalender to javafx.fxml;
-    exports htl.steyr.adventskalender;
-}
